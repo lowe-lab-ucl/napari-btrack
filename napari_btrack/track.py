003ef52245e68e454d925b05ede6bd0c703e7a07
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2022-03-16 21:40:03 +0000

    Adds a time selector for the segementation layer

--- napari_btrack/track.py
+++ napari_btrack/track.py
@@ -38,20 +38,57 @@ def track() -> FunctionGui:
         call_button=True,
         persist=True,
         config_file_path=dict(value=Path.home()),
+        tracking_lower_limit=dict(
+            value=0,
+            label="tracking first layer",
+            min=0,
+            max=10000,
+        ),
+        tracking_upper_limit=dict(
+            value=-1,
+            label="tracking last layer",
+            min=-100,
+            max=10000,
+        ),
         reset_button=dict(widget_type="PushButton", text="Reset defaults"),
     )
     def widget(
         viewer: napari.Viewer,
         segmentation: napari.layers.Image,
         config_file_path: Optional[Path],
+        tracking_lower_limit: int,
+        tracking_upper_limit: int,
         reset_button,
     ):
+        """
+        Parameters
+        ----------
+
+        tracking_upper_limit : int
+            Last element of the segmented layer to analyse. -1 is the last one.
+        """
+
+        if tracking_upper_limit < 0:
+            tracking_upper_limit += segmentation.data.shape[0]
+        if not (
+            tracking_lower_limit < tracking_upper_limit
+            and 0 <= tracking_lower_limit < segmentation.data.shape[0]
+            and tracking_upper_limit < segmentation.data.shape[0]
+        ):
+            raise ValueError(
+                "The time range used for the tracking "
+                f"{tracking_lower_limit}:{tracking_upper_limit} is not valid"
+            )
         segmented_objects = segmentation_to_objects(
-            segmentation.data[:100, ...]
+            segmentation.data[tracking_lower_limit:tracking_upper_limit, ...]
         )
         data, properties, graph = run_tracker(
             segmented_objects, config_file_path
         )
+
+        properties["t"] += tracking_lower_limit
+        data[:, 1] += tracking_lower_limit
+
         viewer.add_tracks(
             data=data,
             properties=properties,
